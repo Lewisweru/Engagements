28f8f5394f127f732a5ee42265f6de9ffb4bce41
lewisweru <codecrafters.mail.com>
2025-03-28 10:20:26 +0300

    Rollback

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBcNrvQ1WqyTuJY-LCjKwpsCQvQiMgfNV0",
+  authDomain: "socialmediamarketplaceke.firebaseapp.com",
+  projectId: "socialmediamarketplaceke",
+  storageBucket: "socialmediamarketplaceke.firebasestorage.app",
+  messagingSenderId: "116564147544",
+  appId: "1:116564147544:web:1406f99b0deaf8c5ecb92d",
+  measurementId: "G-NJEZ35XE7D"
 };
 
 const app = initializeApp(firebaseConfig);
